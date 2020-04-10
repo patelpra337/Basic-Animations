e3c7a92fd0a4d0794251cfc2407007e76459a7c3
Pravin Patel <patelpra337@gmail.com>
2020-04-09 20:52:37 -0500

    Update ViewController

--- BasicAnimation/ViewController.swift
+++ BasicAnimation/ViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class ViewController: UIViewController {
     
     var label: UILabel!
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +18,26 @@ class ViewController: UIViewController {
         configureLabel()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        label.center = view.center
+    }
+    
     func configureLabel() {
         label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
         label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.widthAnchor.constraint(equalTo: label.heightAnchor).isActive = true
+        
+        label.text = "😎"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 96)
+        
+        label.layer.borderWidth = 2
+        label.layer.cornerRadius = 12
+        
     }
     
 
